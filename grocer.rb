506ca59942979dd743221cc37efbc83b2451ff29
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-23 11:27:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -63,6 +63,7 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
   consolidate_cart(cart)
+  apply_coupons(cart, coupons)
   result = 0
   cart.count.times do |index|
     item = cart[index]
